ec19804d3b9cdaf122ea4f875eaef973dd002da8
aemino <aeminodev@gmail.com>
2019-09-22 11:40:37 -0700

    Add more methods to WLDisplay

--- Sources/SwiftWayland/Display.swift
+++ Sources/SwiftWayland/Display.swift
@@ -1,9 +1,37 @@
 import Cwayland
 
 public class WLDisplay {
-    let wlDisplay: OpaquePointer
+    public let pointer: OpaquePointer
+
+    public init?() {
+        guard let wlDisplay = wl_display_create() else {
+            return nil
+        }
+
+        self.pointer = wlDisplay
+    }
+
+    deinit {
+        destroy()
+    }
 
     public init(_ pointer: OpaquePointer) {
-        self.wlDisplay = pointer
+        self.pointer = pointer
+    }
+
+    public func addSocketAutomatically() -> String? {
+        guard let socket = wl_display_add_socket_auto(pointer) else {
+            return nil
+        }
+
+        return String(cString: socket)
+    }
+
+    public func run() {
+        wl_display_run(pointer)
+    }
+
+    public func destroy() {
+        wl_display_destroy(pointer)
     }
 }
